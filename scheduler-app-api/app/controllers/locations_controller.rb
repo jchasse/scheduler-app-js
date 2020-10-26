df6b51ad31f8e3453c8d6015804da0984b9935ae
jchasse <jonathan.r.chasse@gmail.com>
2020-10-26 16:28:29 -0500

    Updated location controller to reflect new Fast JSON serializer

--- scheduler-app-api/app/controllers/locations_controller.rb
+++ scheduler-app-api/app/controllers/locations_controller.rb
@@ -1,3 +1,14 @@
 class LocationsController < ApplicationController
-    
+    def index
+        locations = Location.all
+        render json: LocationSerializer.new(locations).to_serialized_json
+    end
+
+    def show
+        location = Location.find_by(id: params[:id])
+        render json: location.to_json(:include => {
+            :user => {:only => [:first_name, :last_name, :mobile_number, :email]}
+        }, :except => [:updated_at])
+    end
+
 end
