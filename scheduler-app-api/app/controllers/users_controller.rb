df145285b022c10abc7aea30edf882ffaa2d86df
jchasse <jonathan.r.chasse@gmail.com>
2020-10-26 16:28:47 -0500

    Added user serializer and updated controller

--- scheduler-app-api/app/controllers/users_controller.rb
+++ scheduler-app-api/app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        render json: UserSerializer.new(user)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :mobile_number, :email)
+    end
     
 end
