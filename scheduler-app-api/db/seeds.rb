6e043e68aa7cbb5ff266262c2ed63ca8738521ed
jchasse <jonathan.r.chasse@gmail.com>
2020-10-26 14:46:11 -0500

    Added new seed data

--- scheduler-app-api/db/seeds.rb
+++ scheduler-app-api/db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create(first_name: "Bill", last_name: "Thompson", mobile_number: "972-123-4567", email: "bill@test.com")
-location = Location.create(street_address: "123 Main Street", unit: "", zip: "12345", city: "Dallas", state: "TX")
-appointment = Appointment.create(date: "12/31/20", time_target: "3:00", time_window: "2", user_id: user.id, location_id: location.id)
+location = Location.create(street_address: "123 Main Street", unit: "", zip: "12345", city: "Dallas", state: "TX", user_id: user.id)
+service = Service.create(kind: "Electrical", kind_detail: "Repair", issue: "Outlet not working" details: "Location in the kitchen", )
+technician = Technician.create(first_name: "Elec", last_name: "Trician", service_id: service.id)
+appointment = Appointment.create(date: "12/31/20", time_target: "3:00", time_window: "2", location_id: location.id, technician_id: technician.id)
 
