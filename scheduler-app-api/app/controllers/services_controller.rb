d45672fe45a4fa2ef5e688968172ed2cdb972b6d
jchasse <jonathan.r.chasse@gmail.com>
2020-10-27 14:02:57 -0500

    Created services controller

--- scheduler-app-api/app/controllers/services_controller.rb
+++ scheduler-app-api/app/controllers/services_controller.rb
@@ -1,2 +1,19 @@
 class ServicesController < ApplicationController
+
+    def index
+        services = Service.all 
+        render json: ServiceSerializer.new(services)
+    end
+
+    def create
+        service = Service.create(service_params)
+        render json: ServiceSerializer.new(service)
+    end
+
+    private
+
+    def service_params
+        params.require(:service).permit(:kind, :kind_detail, :issue, :details)
+    end
+
 end
