5ed27decdf42226be8dc71b4be86ef69a4817ace
jchasse <jonathan.r.chasse@gmail.com>
2020-10-27 14:01:10 -0500

    Updating to change event listener to step form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,12 @@
-const form = document.getElementById('serviceForm')
+const form = document.getElementById('service-select')
 
-form.addEventListener('submit', Service.createService)
+form.addEventListener('click', Service.createService)
 
+
+function titlecase(string) {
+    let words = string.split(' ');
+    for (let n = 0; n < words.length; n++) {
+      words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
+    }
+    return words.join(' ');
+}
