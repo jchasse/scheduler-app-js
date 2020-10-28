50485db1312f0708dd091edaefc4d73e34da3f11
jchasse <jonathan.r.chasse@gmail.com>
2020-10-28 16:27:35 -0500

    Added create route method for location

--- scheduler-app-api/app/controllers/locations_controller.rb
+++ scheduler-app-api/app/controllers/locations_controller.rb
@@ -1,14 +1,28 @@
 class LocationsController < ApplicationController
     def index
         locations = Location.all
-        render json: LocationSerializer.new(locations).to_serialized_json
+        render json: locations
+        # render json: LocationSerializer.new(locations).to_serialized_json
     end
+
+    def create
+        byebug
+        location = Location.create(location_params)
+        render json: LocationSerializer.new(location)
+    end
+
 
     def show
         location = Location.find_by(id: params[:id])
         render json: location.to_json(:include => {
             :user => {:only => [:first_name, :last_name, :mobile_number, :email]}
         }, :except => [:updated_at])
+    end
+
+    private
+
+    def location_params
+        params.require(:location).permit(:street_addres, :unit, :city, :state, :zip)
     end
 
 end
