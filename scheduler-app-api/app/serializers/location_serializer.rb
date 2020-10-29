868e827ac4be79ce0548b17ef329ed970cbe183b
jchasse <jonathan.r.chasse@gmail.com>
2020-10-29 09:16:44 -0500

    Added user_id to location serializer

--- scheduler-app-api/app/serializers/location_serializer.rb
+++ scheduler-app-api/app/serializers/location_serializer.rb
@@ -1,4 +1,4 @@
 class LocationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :street_address, :unit, :city, :state, :zip
+  attributes :street_address, :unit, :city, :state, :zip, :user_id
 end
