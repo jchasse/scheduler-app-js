79e5c90191f8aedc287f84a7b49c1fb4b524fd00
jchasse <jonathan.r.chasse@gmail.com>
2020-10-29 09:16:26 -0500

    Updated create and index functions

--- scheduler-app-api/app/controllers/users_controller.rb
+++ scheduler-app-api/app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
+    def index
+        users = User.all
+        render json: UserSerializer.new(users)
+        # render json: LocationSerializer.new(locations).to_serialized_json
+    end
+
     def create
         user = User.create(user_params)
         render json: UserSerializer.new(user)
