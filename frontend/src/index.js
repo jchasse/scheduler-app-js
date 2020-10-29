2abf99ceeae7da09a4b2ea35c4787356e273163d
jchasse <jonathan.r.chasse@gmail.com>
2020-10-29 09:18:13 -0500

    Removed titlecase method - no longer needed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,6 @@ const serviceSelect = document.getElementById('category-select')
 serviceSelect.addEventListener("change", function() {
     Service.renderIssueForm(serviceSelect.value)
     Service.renderDetailsTab()
-    Service.renderPersonTab()
+    User.renderUserTab()
+    Location.renderLocationTab()
 })
-
-
-function titlecase(string) {
-    let words = string.split(' ');
-    for (let n = 0; n < words.length; n++) {
-      words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
-    }
-    return words.join(' ');
-}
