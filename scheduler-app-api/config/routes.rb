2cd622bbbdfd545c324310ba11882dbb5dc10274
jchasse <jonathan.r.chasse@gmail.com>
2020-10-31 23:06:47 -0500

    Nested routes locations under users

--- scheduler-app-api/config/routes.rb
+++ scheduler-app-api/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :technicians
   resources :services
   resources :appointments
-  resources :locations
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :locations
+  # resources :users
+
+  resources :users do
+    resources :locations
+  end
 end
